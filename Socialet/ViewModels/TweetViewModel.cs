60f23e5bc6fa00b1d3fc9ba0dfca47e47ad35de5
Zeeshan Ahmed <zish@hotmail.co.uk>
2013-11-05 15:44:06 +0000

    Added Twitter profile image

--- Socialet/ViewModels/TweetViewModel.cs
+++ Socialet/ViewModels/TweetViewModel.cs
@@ -10,5 +10,7 @@ namespace Socialet.ViewModels
 
         public string UserName { get; set; }
         public string StatusNumber { get; set; }
+
+        public string ProfileImageUrl { get; set; }
     }
 }
